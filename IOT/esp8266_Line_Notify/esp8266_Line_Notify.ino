aa28099d350f531e32eafec5f34a2f0d04b55030
WATCHARAPHON  PAMAYAYANG <94774854+WATCHARAPHON69@users.noreply.github.com>
2022-02-16 20:06:15 +0700

    Add files via upload

---
+++ IOT/esp8266_Line_Notify/esp8266_Line_Notify.ino
@@ -0,0 +1,29 @@
+#include "EEPROM.h"
+#include <ESP8266WiFi.h>
+#include <TridentTD_LineNotify.h>
+void setup() {
+  Serial.begin(115200);
+  delay(3000);
+  EEPROM.begin(512);
+  //setwifi();
+  pinMode(D0, INPUT_PULLUP);
+  pinMode(D1, INPUT_PULLUP);
+  delay(3000);
+  Serial.println("START");
+  if (digitalRead(D0) == 0) {
+    Serial.println("AP MODE");
+    setwifi();
+  } else {
+    Serial.println("CL MODE");
+    Connect();
+  }
+}
+void loop() {
+  if (digitalRead(D1) == 0) {
+
+
+    LINE.notifyPicture("ตรวจพบความเคลื่อนไหว", "https://github.com/WATCHARAPHON69/IOT/blob/main/IOT/esp8266_Line_Notify/img.jpg");
+    Serial.println("ตรวจพบความเคลื่อนไหว");
+    while (digitalRead(D1) == 0);
+  }
+}
